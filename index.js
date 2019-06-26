96fd84685aeb537c340f2228a3e73e20bbe46bc3
GiuliaGallorini <giuliagallorini7@gmail.com>
2019-06-26 18:06:12 +0100

    Finished with correction

--- index.js
+++ index.js
@@ -12,21 +12,46 @@ mongoose
     console.error("Error connecting to mongo", err);
   });
 
-Recipe.deleteMany({}).then(() => {
-  
-  Recipe.create(data)
-    .then(recipe => {
-      for (let n = 0; n < recipe.length; n++)
-      {console.log("The title of the recipe is: " + recipe[n].title)};
+// First: delete all recipes from the database
+Recipe.deleteMany().then(() => {
+  // Iteration 2 create the recipe
+  Recipe.create({
+    title: "Pastel de Nata",
+    cuisine: "Portuguese",
+    ingredients: ["cream", "sugar", "eggs", "dough"]
+  }).then(recipe => {
+    console.log(`The recipe "${recipe.title}" has been created `);
+  });
+
+  // Iteration 3: also works with create()
+  Recipe.insertMany(data).then(recipes => {
+      console.log("These are the titles of the recipes");
+      for (let n = 0; n < recipes.length; n++) {
+        console.log("- " + recipes[n].title);
+      }
+
+      // Iteration 4
+      Recipe.updateOne(
+        { title: "Rigatoni alla Genovese" },
+        { duration: 100 }
+      ).then(res => {
+        console.log(
+          `The update is succesful and updated ${res.nModified} recipe(s)`
+        );
+      });
+
+      // Iteration 5
+      Recipe.deleteOne({ name: "Carrot Cake" }).then(res => {
+        console.log(`The "Carrot Cake" is deleted`, res);
+
+        // Iteration 6
+        // Close the connection to the database
+        // Optional (it is cleaner)
+        mongoose.connection.close();
+      });
     })
+
     .catch(err => {
-      console.error("Error connecting to mongo", err);
+      console.error("An error happened", err);
     });
-
-    Recipe.updateOne({ title: "Rigatoni alla Genovese"}, { duration: 100 })
-  .then(successCallback)
-  .catch(errorCallback);
-
 });
-
-
